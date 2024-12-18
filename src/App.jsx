c5231339bed21a53c3f311f2d8f41716303e6761
Berlin Ortega <marlitros200002@gmail.com>
2024-12-17 20:16:00 -0600

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import { useState } from 'react'
 
-export default function App() {
-  const isGoingOut = false
-  return (
-    <>
-      <h1>Do i feel like going out tonight</h1>
-      <button>{(isGoingOut) ? 'Yes' : 'No'}</button>
-    </>
+export default function App(){
+  const[isGoingout, setIsGoingOut] = useState(false)
+
+  function changeMing(){
+    setIsGoingOut(prev => !prev)
+  }
+
+  return(
+    <main>
+      <h1>Do i feel like going out tonight?</h1>
+      <button 
+        onClick={changeMing}
+        className='value'
+        aria-label={`Current answer is ${isGoingout ? "Yes" : "No"}. Click to change it.`}
+      >{isGoingout ?  "Yes" : "No"}</button>
+    </main>
   )
 }
+
+
+//export default function App() {
+//  const isGoingOut = false
+//  return (
+//    <>
+//      <h1>Do i feel like going out tonight</h1>
+//      <button>{(isGoingOut) ? 'Yes' : 'No'}</button>
+//    </>
+//  )
+//}
